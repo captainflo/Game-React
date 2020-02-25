afad608a2e705497dd7dbd0be7b4d1365fa2a563
florian lahitte <lahitte.florian@gmail.com>
2020-02-25 09:47:05 -0500

    commit for heroku

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,7 +1,6 @@
 // prod.js - production keys here!!!
 module.exports = {
   siteUrl: 'https://wikivideogame.herokuapp.com',
-  api: 'fe13acf4e9784752ad6ab499921d74a8',
   cloudinaryClientName: 'dwtc6zep7',
   cloudinaryClientID: '596555837329564',
   cloudinaryClientSecret: 'oCn37LxVL2KbWdFIU_H89yVcz6o'
