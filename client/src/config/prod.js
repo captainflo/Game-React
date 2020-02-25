7ba790e7a88cc4a6abfefece70854067cd1c2cf0
florian lahitte <lahitte.florian@gmail.com>
2020-02-25 09:33:01 -0500

    init server heroku

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,6 +1,6 @@
 // prod.js - production keys here!!!
 module.exports = {
-  siteUrl: 'https://wikisport.herokuapp.com',
+  siteUrl: 'https://wikivideogame.herokuapp.com',
   api: 'fe13acf4e9784752ad6ab499921d74a8',
   cloudinaryClientName: 'dwtc6zep7',
   cloudinaryClientID: '596555837329564',
